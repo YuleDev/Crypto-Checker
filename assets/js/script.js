bdbd8506abab3679252522dffcb4042e74c3431e
thatblondetitan <koleervine@gmail.com>
2022-01-25 15:46:51 -0700

    additional url in JS
    added rapidAPI url to JS comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ cURL for searchbar = ('curl https://api.coinranking.com/v2/search-suggestions?qu
 -H 'x-access-token: your-api-key' \
 -G');
 found on ('https://developers.coinranking.com/api/documentation');
+can also be found on ('https://rapidapi.com/Coinranking/api/coinranking1/')
 
 cURL for CoinMarketCap = ('curl -H "X-CMC_PRO_API_KEY: b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c" -H "Accept: application/json" -d "start=1&limit=5000&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest');
 Want to get latest data here? = ('https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest');
