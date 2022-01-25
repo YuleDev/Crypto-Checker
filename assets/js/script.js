631d54176b4f5093464637e5163b5a8f5813e19d
thatblondetitan <koleervine@gmail.com>
2022-01-25 15:14:15 -0700

    JS comments
    added comments for the api's and the documentation/url's

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/* Base url for CoinGecko = ('api.coingecko.com/api/v3'');
+CoinGecko for searchbar = ('/search')
+found on ('https://www.coingecko.com/api/documentations/v3') and ('https://www.coingecko.com/en/api/documentation');
+
+Base url for CoinRanking = ('https://api.coinranking.com/v2');
+cURL for searchbar = ('curl https://api.coinranking.com/v2/search-suggestions?query=bitco \
+-H 'x-access-token: your-api-key' \
+-G');
+found on ('https://developers.coinranking.com/api/documentation');
+
+cURL for CoinMarketCap = ('curl -H "X-CMC_PRO_API_KEY: b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c" -H "Accept: application/json" -d "start=1&limit=5000&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest');
+Want to get latest data here? = ('https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest');
+found on ('https://coinmarketcap.com/api/documentation/v1/#section/Quick-Start-Guide');
+
+Binance seems to have alot of utility, the problem I encounterd is that in order to be used
+you need a Binance account and a corresponding AuthKey to actually get data */
