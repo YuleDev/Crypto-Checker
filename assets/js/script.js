4424acf58441d4814f2278e83629187da11da7cf
thatblondetitan <koleervine@gmail.com>
2022-01-26 10:26:58 -0700

    js additions
    added fetch for top price crypto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* Base url for CoinGecko = ('api.coingecko.com/api/v3'');
+/* Base url for CoinGecko = ('api.coingecko.com/api/v3');
 CoinGecko for searchbar = ('/search')
 found on ('https://www.coingecko.com/api/documentations/v3') and ('https://www.coingecko.com/en/api/documentation');
 can also be found on ('https://rapidapi.com/coingecko/api/coingecko/');
@@ -9,6 +9,7 @@ cURL for searchbar = ('curl https://api.coinranking.com/v2/search-suggestions?qu
 -G');
 found on ('https://developers.coinranking.com/api/documentation');
 can also be found on ('https://rapidapi.com/Coinranking/api/coinranking1/');
+API KEY -- coinranking3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830
 
 cURL for CoinMarketCap = ('curl -H "X-CMC_PRO_API_KEY: b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c" -H "Accept: application/json" -d "start=1&limit=5000&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest');
 Want to get latest data here? = ('https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest');
@@ -17,3 +18,55 @@ can also be found on ('https://rapidapi.com/zakutynsky/api/CoinMarketCap/');
 
 Binance seems to have alot of utility, the problem I encounterd is that in order to use the API
 you need a Binance account and a corresponding AuthKey to actually get data */
+
+
+
+/* Below is the search suggestion api from coinranking */
+
+/* const searchSuggest = new Request("https://coinranking1.p.rapidapi.com/search-suggestions?referenceCurrencyUuid=yhjMzLPhuIDl") {
+    method: 'GET',
+    headers: {
+        "x-access-key": "3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830",
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+		"x-rapidapi-key": "f112fe985emsh7fd3ffcf23a79fbp14c66djsnd4ab259a0c95"
+	},
+    mode: 'cors',
+    cache: 'default',
+};
+
+fetch("https://coinranking1.p.rapidapi.com/search-suggestions?referenceCurrencyUuid=yhjMzLPhuIDl", {
+    "method": "GET",
+	"headers": {
+        "x-access-key": "3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830",
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+		"x-rapidapi-key": "f112fe985emsh7fd3ffcf23a79fbp14c66djsnd4ab259a0c95"
+	}
+})
+.then(function (response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+        })
+    }
+}); */
+
+fetch("https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=price&orderDirection=desc&limit=16&offset=0", {
+	"method": "GET",
+	"headers": {
+        "x-access-key": "3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830",
+		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+		"x-rapidapi-key": "f112fe985emsh7fd3ffcf23a79fbp14c66djsnd4ab259a0c95"
+	}
+})
+.then(function (response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+        })
+    }
+});
+
+/* for (var i = 0; i < data.length; i++) {
+                    // console.log(data[i].market_cap);
+                    console.log(data[i].price);
+                    } */
