75ed8971d5e644fa09692f4d7a82ad3d41cd91b5
thatblondetitan <koleervine@gmail.com>
2022-01-27 09:52:02 -0700

    js and html update
    added id's to html and dom-selectors to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,39 +18,105 @@ can also be found on ('https://rapidapi.com/zakutynsky/api/CoinMarketCap/');
 
 Binance seems to have alot of utility, the problem I encounterd is that in order to use the API
 you need a Binance account and a corresponding AuthKey to actually get data */
+var idNameEl = document.querySelector("#name0");
+var idPriceEl = document.querySelector("#price0");
+var idImgEl = document.querySelector("#img0");
+var idNameEl = document.querySelector("#name1");
+var idPriceEl = document.querySelector("#price1");
+var idImgEl = document.querySelector("#img1");
+var idNameEl = document.querySelector("#name2");
+var idPriceEl = document.querySelector("#price2");
+var idImgEl = document.querySelector("#img2");
+var idNameEl = document.querySelector("#name3");
+var idPriceEl = document.querySelector("#price3");
+var idImgEl = document.querySelector("#img3");
+var idNameEl = document.querySelector("#name4");
+var idPriceEl = document.querySelector("#price4");
+var idImgEl = document.querySelector("#img4");
+var idNameEl = document.querySelector("#name5");
+var idPriceEl = document.querySelector("#price5");
+var idImgEl = document.querySelector("#img5");
+var idNameEl = document.querySelector("#name6");
+var idPriceEl = document.querySelector("#price6");
+var idImgEl = document.querySelector("#img6");
+var idNameEl = document.querySelector("#name7");
+var idPriceEl = document.querySelector("#price7");
+var idImgEl = document.querySelector("#img7");
 
-var apiUrl = "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=price&orderDirection=desc&limit=8&offset=0";
+var getMainIndex = function () {
 
-fetch(apiUrl, {
-    "method": "GET",
-    "headers": {
-        "x-access-key": "3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830",
-        "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-        "x-rapidapi-key": "f112fe985emsh7fd3ffcf23a79fbp14c66djsnd4ab259a0c95"
-    }
-})
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
+    var apiUrl = "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=price&orderDirection=desc&limit=8&offset=0";
+
+    fetch(apiUrl, {
+        "method": "GET",
+        "headers": {
+            "x-access-key": "3c29bda109d4290191bea7abecd0074bfe38d5634e0e6830",
+            "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+            "x-rapidapi-key": "f112fe985emsh7fd3ffcf23a79fbp14c66djsnd4ab259a0c95"
         }
-    }).then(function (cryptoInfo) {
+    })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        }).then(function (cryptoInfo) {
+            displayMainIndex(cryptoInfo.data.coins);
+        });
+    };
+    
+    //adds commas to the current price
+    function separator(numb) {
+    var str = numb.toString().split(".");
+    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str.join(".");
+};
 
-        const seanArr = []
-        for (var i = 0; i < cryptoInfo.data.coins.length; i++) {
-            seanArr.push(cryptoInfo.data.coins[i]);
-        }
-        console.log(seanArr);
-        (seanArr).forEach(function (coin) {
-        console.log(coin)
-        })
-    });
+var displayMainIndex = function (data) {
+    console.log(data);
+    var numberBeauty = Math.floor(data[0].price);
+    idNameEl.textContent = data[0].name;
+    idPriceEl.textContent = separator(numberBeauty);
+    idImgEl.setAttribute("src", data[0].iconUrl);
+    
+    /* for (var i = 0; i < data.length; i++) {
+    console.log(data[i].iconUrl);
+    console.log(data[i].name);
+    console.log(data[i].price);
+    } */
+};
+getMainIndex();
+
+
+
+
+
+/* previously used code that may be useful for the future */
+
+/* seanArr went beow displaymainindex on line 63
+ const seanArr = []
+seanArr.push(cryptoInfo.data.coins[i]);
+}
+/*console.log(seanArr);
+(seanArr).forEach(function (coin) {
+console.log(coin)
+}) */
+
+
+/* for (var i = 0; i < data.length; i++) {
+    //Creating a h3 element and a p element
+    var userName = document.createElement('h3');
+    var userUrl = document.createElement('p');
+
+    //Setting the text of the h3 element and p element.
+    userName.textContent = data[i].login;
+    userUrl.textContent = data[i].url;
+
+    //Appending the dynamically generated html to the div associated with the id="users"
+    //Append will attach the element as the bottom most child.
+    usersContainer.append(userName);
+    usersContainer.append(userUrl);
+  }; */
 
 /* let coinPrice = cryptoInfo.data.coins[i].price;
 let coinIcon = cryptoInfo.data.coins[i].iconUrl;
 let coinRank = cryptoInfo.data.coins[i].rank; */
-
-
-/* grab each coin 0-8 out of the array
-have each coin only show the data of: name, price, iconUrl
-have the coins descend from price
-have the coins append or fill text to each card based on ID */
